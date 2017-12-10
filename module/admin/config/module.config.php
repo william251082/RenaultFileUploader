998e38cb9cde76bd042e22654a64a0b22e8a3d47
williamdelrosario <williamdelrosario@yahoo.com>
2017-12-10 15:39:19 +0100

    Modules, Routing and Controllers setup2

---
+++ module/admin/config/module.config.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace Admin;
+
+use Zend\Router\Http\Segment;
+use Zend\ServiceManager\Factory\InvokableFactory;
+
+return [
+    'controllers' => [
+        'factories' => [
+            Controller\AdminController::class => InvokableFactory::class,
+        ],
+    ],
+
+    // The following section is new and should be added to your file:
+    'router' => [
+        'routes' => [
+            'admin' => [
+                'type'    => Segment::class,
+                'options' => [
+                    'route' => '/admin[/:action[/:id]]',
+                    'constraints' => [
+                        'action' => '[a-zA-Z][a-zA-Z0-9_-]*',
+                        'id'     => '[0-9]+',
+                    ],
+                    'defaults' => [
+                        'controller' => Controller\AdminController::class,
+                        'action'     => 'index',
+                    ],
+                ],
+            ],
+        ],
+    ],
+
+    'view_manager' => [
+        'template_path_stack' => [
+            'admin' => __DIR__ . '/../view',
+        ],
+    ],
+];
---
+++ module/admin/src/Controller/AdminController.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Admin\Controller;
+
+use Zend\Mvc\Controller\AbstractActionController;
+use Zend\View\Model\ViewModel;
+
+class AdminController extends AbstractActionController
+{
+    public function indexAction()
+    {
+    }
+
+    public function addAction()
+    {
+    }
+
+    public function editAction()
+    {
+    }
+
+    public function deleteAction()
+    {
+    }
+}
